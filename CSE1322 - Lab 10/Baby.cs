3339b3fa6c0cfc7d51037a69a58507e86e104731
BorkBarkBork <91920737+BorkBarkBork@users.noreply.github.com>
2026-09-03 15:29:05 -0400

    CSE 1322 - Lab 10

---
+++ CSE1322 - Lab 10/Baby.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+class Baby
+{
+    private int time;
+    private string name;
+
+    public Baby(string name)
+    {
+        this.name = name;
+        Random random = new Random();
+        this.time = random.Next(5000);
+    }
+
+    public void Run()
+    {
+        try
+        {
+            Console.WriteLine("My name is " + this.name + " and I am going to sleep for " + this.time + "ms");
+            Thread.Sleep(time);
+            Console.WriteLine("My name is " + this.name + " and I'm awake, feed me!!!");
+        }
+        catch (ThreadInterruptedException tie) 
+        {
+            Console.WriteLine(tie.Message);
+        }
+    }
+}
---
+++ CSE1322 - Lab 10/Driver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+class Driver
+{
+    public static void Main(string[] args)
+    {
+        Baby b1 = new Baby("Noah");
+        Baby b2 = new Baby("Olivia");
+        Baby b3 = new Baby("Liam");
+        Baby b4 = new Baby("Emma");
+        Baby b5 = new Baby("Amelia");
+
+        Thread t1 = new Thread(b1.Run);
+        Thread t2 = new Thread(b2.Run);
+        Thread t3 = new Thread(b3.Run);
+        Thread t4 = new Thread(b4.Run);
+        Thread t5 = new Thread(b5.Run);
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+        t4.Start();
+        t5.Start();
+    }
+}
